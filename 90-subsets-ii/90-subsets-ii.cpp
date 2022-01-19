8cc45f19ff9cc1ab8c3f68ab865f4a1dc1cdc171
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-01-19 20:10:58 +0530

    Time: 12 ms (15.29%), Space: 17.6 MB (8.19%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -3,25 +3,30 @@ public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) 
     {
         sort(nums.begin(),nums.end());
-        set <vector <int> > res;
+        vector <vector <int> > res;
         vector<int> op;
         solve(nums,op,res);
-        vector < vector < int> > ans;
-        copy(res.begin(),res.end(),back_inserter(ans));
-        return ans;
+        // vector < vector < int> > ans;
+        // copy(res.begin(),res.end(),back_inserter(ans));
+        return res;
     }
-    void solve (vector<int> ip,vector <int> op,set<vector<int>> &res)
+    void solve (vector<int> ip,vector <int> op,vector<vector<int> > &res)
     {
         if (ip.size()==0)
         {
-            res.insert(op);
+            res.push_back(op);
             return;
         }
         vector <int> op1 =op;
         vector <int> op2 =op;
-        op2.push_back(ip[ip.size()-1]);
+        int top=ip[ip.size()-1];
+        op2.push_back(top);
         ip.pop_back();
-        solve(ip,op1,res);
         solve(ip,op2,res);
+        while(ip.size()>0 && ip[ip.size()-1]==top)
+        {
+            ip.pop_back();
+        }
+        solve(ip,op1,res);
     }
 };
