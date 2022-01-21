8f3208ea99a333f05506dfb85608f870ed8f2edd
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-01-21 11:19:47 +0530

    Time: 75 ms (17.07%), Space: 6.4 MB (81.79%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) 
+    {
+        solve(board);
+    }
+    bool solve(vector<vector<char>>& board)
+    {
+        for (int i=0;i<9;i++)
+        {
+            for (int j=0;j<9;j++)
+            {
+                if (board[i][j]=='.')
+                {
+                    for (int k=0;k<9;k++)
+                    {
+                        if (valid(board,i,j,'1'+k))
+                        {
+                            board[i][j]='1'+k;
+                            if (solve(board)==true)
+                            {
+                                return true;
+                            }
+                            else
+                            {
+                                board[i][j]='.';
+                            }
+                        }
+                    }
+                    return false;
+                }                
+            }
+        }
+        return true;
+    }
+    bool valid(vector<vector<char> > &board,int row,int col,char c)
+    {
+        for (int i=0;i<9;i++)
+        {
+            if (board[i][col]==c || board[row][i]==c)
+            {
+                return false;
+            }
+            if (board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
