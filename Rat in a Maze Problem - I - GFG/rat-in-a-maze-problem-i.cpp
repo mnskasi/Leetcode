d0f5508d448e6cb1aa7ca4318018019e47334819
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-01-25 13:19:36 +0530

    Added solution - LeetHub

--- Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
+++ Rat in a Maze Problem - I - GFG/rat-in-a-maze-problem-i.cpp
@@ -8,61 +8,63 @@ using namespace std;
  // } Driver Code Ends
 // User function template for C++
 
+
 class Solution{
-    void solve(int i, int j, vector<vector<int>> &a, int n, vector<string> &ans, string move, 
-    vector<vector<int>> &vis, int di[], int dj[]) {
-        if(i==n-1 && j==n-1) {
-            ans.push_back(move);
-            return; 
-        }
-        string dir = "DLRU"; 
-        for(int ind = 0; ind<4;ind++) {
-            int nexti = i + di[ind]; 
-            int nextj = j + dj[ind]; 
-            if(nexti >= 0 && nextj >= 0 && nexti < n && nextj < n && !vis[nexti][nextj] && a[nexti][nextj] == 1) {
-                vis[i][j] = 1; 
-                solve(nexti, nextj, a, n, ans, move + dir[ind], vis, di, dj);
-                vis[i][j] = 0; 
-            }
-        }
-        // downward
-        // if(i+1<n && !vis[i+1][j] && a[i+1][j] == 1) {
-        //     vis[i][j] = 1; 
-        //     solve(i+1, j, a, n, ans, move + 'D', vis);
-        //     vis[i][j] = 0; 
-        // }
-        
-        // // left
-        // if(j-1>=0 && !vis[i][j-1] && a[i][j-1] == 1) {
-        //     vis[i][j] = 1; 
-        //     solve(i, j-1, a, n, ans, move + 'L', vis);
-        //     vis[i][j] = 0; 
-        // }
-        
-        // // right 
-        // if(j+1<n && !vis[i][j+1] && a[i][j+1] == 1) {
-        //     vis[i][j] = 1; 
-        //     solve(i, j+1, a, n, ans, move + 'R', vis);
-        //     vis[i][j] = 0; 
-        // }
-        
-        // // upward
-        // if(i-1>=0 && !vis[i-1][j] && a[i-1][j] == 1) {
-        //     vis[i][j] = 1; 
-        //     solve(i-1, j, a, n, ans, move + 'U', vis);
-        //     vis[i][j] = 0; 
-        // }
-    }
     public:
-    vector<string> findPath(vector<vector<int>> &m, int n) {
-        vector<string> ans;
-        vector<vector<int>> vis(n, vector<int> (n, 0)); 
-        int di[] = {+1, 0, 0, -1}; 
-        int dj[] = {0, -1, 1, 0}; 
-        if(m[0][0] == 1) solve(0,0,m,n, ans, "", vis, di, dj); 
-        return ans; 
+    void solve(vector< vector <int> > m,vector<string> & res,string path,int n, int i,int j)
+    {
+        if (i==n-1 && j==n-1)
+        {
+            res.push_back(path);
+            return ;
+        }
+        if (i!=n-1 && m[i+1][j]!=0)
+        {
+            path+="D";
+            m[i][j]=0;
+            solve(m,res,path,n,i+1,j);
+            m[i][j]=1;
+            path.pop_back();
+        }
+        if (j!=0 && m[i][j-1]!=0)
+        {
+            path+="L";
+            m[i][j]=0;
+            solve(m,res,path,n,i,j-1);
+            m[i][j]=1;
+            path.pop_back();
+        }
+        if (j!=n-1 && m[i][j+1]!=0)
+        {
+            path+="R";
+            m[i][j]=0;
+            solve(m,res,path,n,i,j+1);
+            m[i][j]=1;
+            path.pop_back();
+        }
+        if (i!=0 && m[i-1][j]!=0)
+        {
+            path+="U";
+            m[i][j]=0;
+            solve(m,res,path,n,i-1,j);
+            m[i][j]=1;
+            path.pop_back();
+        }
+    }
+    vector<string> findPath(vector<vector<int>> &m, int n) 
+    {
+        vector<string> res;
+        string path="";
+        if (m[n-1][n-1]==0 || m[0][0]==0)
+        {
+            return res;
+        }
+        solve(m,res,path,n,0,0);
+        return res;
+        // Your code goes here
     }
 };
+    
 
 
 // { Driver Code Starts.
