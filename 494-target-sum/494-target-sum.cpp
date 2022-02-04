f7dbc509de911bd49d845027bf5a853e91a8e13a
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-02-04 23:00:28 +0530

    Time: 24 ms (60.36%), Space: 9 MB (76.38%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) 
+    {
+        int sum=0;
+        for (int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+        }
+        if ((sum-target)%2!=0 || sum-target < 0)
+        {
+            return 0;
+        }
+        int k=(sum-target)/2;
+        int dp[nums.size()+1][k+1];
+        for (int i=0;i<=k;i++)
+        {
+            dp[0][i]=0;
+        }
+        for (int i=0;i<=nums.size();i++)
+        {
+            dp[i][0]=1;
+        }
+        for (int i=1;i<=nums.size();i++)
+        {
+            for (int j=0;j<=k;j++)
+            {
+                if (nums[i-1]<=j)
+                {
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.size()][k];
+    }
+};
