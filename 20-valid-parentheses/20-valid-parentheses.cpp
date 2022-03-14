84267f21c646d28974583c46d16261588a1aa952
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-03-14 14:39:34 +0530

    Time: 3 ms (35.67%), Space: 6.3 MB (51.42%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        for (int i=0;i<s.size();i++)
+        {
+            if (s[i]=='(' or s[i]=='[' or s[i]=='{')
+            {
+                st.push(s[i]);
+            }
+            else if (s[i]==')')
+            {
+                if (st.size()!=0 and st.top()=='(')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (s[i]=='}')
+            {
+                if (st.size()!=0 and st.top()=='{')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (s[i]==']')
+            {
+                if (st.size()!=0 and st.top()=='[')
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if (st.size()==0) return true;
+        return false;
+    }
+};
