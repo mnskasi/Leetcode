d480ecc38a86fc8c8cd7b8510909e3960a8cd00d
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-03-29 09:20:30 +0530

    Time: 0 ms (100.00%), Space: 6 MB (55.45%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -6,14 +6,15 @@ public:
         {
             return n;
         }
-        int arr[n+1];
-        arr[1]=1;
-        arr[2]=2;
+        int prev1=2;
+        int prev2=1;
         for (int i=3;i<=n;i++)
         {
-            arr[i]=arr[i-1]+arr[i-2];
+            int act=prev1+prev2;
+            prev2=prev1;
+            prev1=act;
         }
-        return arr[n];
+        return prev1;
         
     }
 };
