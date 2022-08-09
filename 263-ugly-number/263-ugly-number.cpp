fe54cd289231328ed8f0f126e4f3c1508245b3ce
Kasi Viswanath Munagala <mnskasiviswanath@gmail.com>
2022-08-09 21:30:50 +0530

    Time: 3 ms (58.59%), Space: 5.9 MB (69.58%) - LeetHub

--- 263-ugly-number/263-ugly-number.cpp
+++ 263-ugly-number/263-ugly-number.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool isUgly(int n) {
-        if (n == 0) return false;
-        if (n < 0) {
+        if (n <= 0) {
             return false;
         }
         while(n%2 == 0) {
